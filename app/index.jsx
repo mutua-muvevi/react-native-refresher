a511fc8e74268e1110743340577d6448a026cb19
mutua-muvevi <samstore637@gmail.com>
2024-08-19 03:20:56 +0300

    folder structure

--- app/index.jsx
+++ app/index.jsx
@@ -1,16 +1,29 @@
 import { StatusBar } from "expo-status-bar";
-import { Text, View } from "react-native";
+import { ScrollView, Text, View } from "react-native";
 import { Link } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { images } from "../constants";
+import { Image } from "react-native";
 
 export default function App() {
 	return (
-		<View className="flex-1 items-center justify-center bg-white">
-			<Text className="text-3xl font-pblack">JSM</Text>
-			<StatusBar style="auto" />
-			<Link href="/profile" style={{color: "blue"}}>
-				Go to profile
-			</Link>
-		</View>
+		<SafeAreaView className="bg-primary h-full">
+			<ScrollView contentContainerStyle={{ height: "100%" }}>
+				<View className="w-full justify-center items-center h-full px-4">
+					<Image
+						source={images.logo}
+						className="w-[130px] h-[130px]"
+						resizeMode="contain"
+						/>
+
+					<Image
+						source={images.cards}
+						className="max-w-[380px] w-full h-[300px]"
+						resizeMode="contain"
+					/>
+				</View>
+			</ScrollView>
+		</SafeAreaView>
 	);
 }
 
